e5f73d13fc6b6a55f7f83db6b586d89018f1ec96
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-20 10:44:04 +0100

    archivos añadidos

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <div>
+      <span>Logo</span>
+      <form action="">
+        <input type="text" name="" id="" />
+        <button></button>
+      </form>
+      
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/NavbarIcons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavbarIcons() {
+  return (
+    <div>NavbarIcons</div>
+  )
+}
+
+export default NavbarIcons
