4a0d5a0bcecc0a09db38145ce77365bda547d670
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-03-06 19:20:54 +0100

    Nuevos cambios ultima sección

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import MainContent from '../components/MainContent'
 import Navbar from '../components/Navbar'
 import { initNotifications, initContentCart } from "../components/NavbarData"
-import { useUserStates } from '../hooks/useUserStates'
 
 function Home({states,callbacks,numberOfCartItems,numberOfNotifications}) {
 
