da93a5b4af0720d54a9842fe04d74c9f81cffb14
edi vidrascu <edividrascu@gmail.com>
2024-03-07 13:24:31 +0100

    flechas dislexicas

--- src/components/HowToCardButtom.jsx
+++ src/components/HowToCardButtom.jsx
@@ -8,9 +8,15 @@ function HowToCardButtom() {
             <text className="HowToCardButtom-text-2 text11" x="8.5%" y="14%"  fill="#EDB72D"   color="#EDB72D">
             Your Deck to Cash 
           </text>
+          {/* FELCHA */}
+          <svg xmlns="http://www.w3.org/2000/svg" className='FlechaParabajo-top-3' x="29%" y="22%" viewBox="0 0 24 24" width="60" height="60"><path fill="none" stroke="#c78d18" strokeLinecap="round" strokeLinejoin="round" d="M3.5 4.91h0a1.41 1.41 0 0 0 .41 1L15 17l-6.91-.41A1.51 1.51 0 0 0 6.5 18.1h0a1.51 1.51 0 0 0 1.4 1.5l12.6.9-.9-12.6a1.51 1.51 0 0 0-1.5-1.4h0a1.51 1.51 0 0 0-1.5 1.59L17 15 5.91 3.91a1.41 1.41 0 0 0-1-.41h0A1.41 1.41 0 0 0 3.5 4.91Z" data-name="Arrow 9 Left Down" ></path></svg>
+
           <text className="HowToCardButtom-text-2 text22" x="17.2%" y="55%"  fill="#EDB72D"   color="#EDB72D">
           Trade Magic, Profit!
           </text>
+          {/* FELCHA 2*/}
+          <svg xmlns="http://www.w3.org/2000/svg" className='FlechaParabajo-top-4' x="48.5%" y="64%" viewBox="0 0 24 24" width="60" height="60"><path fill="none" stroke="#c78d18" strokeLinecap="round" strokeLinejoin="round" d="M3.5 4.91h0a1.41 1.41 0 0 0 .41 1L15 17l-6.91-.41A1.51 1.51 0 0 0 6.5 18.1h0a1.51 1.51 0 0 0 1.4 1.5l12.6.9-.9-12.6a1.51 1.51 0 0 0-1.5-1.4h0a1.51 1.51 0 0 0-1.5 1.59L17 15 5.91 3.91a1.41 1.41 0 0 0-1-.41h0A1.41 1.41 0 0 0 3.5 4.91Z" data-name="Arrow 9 Left Down" ></path></svg>
+          
           <text className="HowToCardButtom-text-2 text33" x="9%" y="95%"  fill="#EDB72D"   color="#EDB72D">
           Boost your earnings
           </text>
--- src/components/HowToCardTop.jsx
+++ src/components/HowToCardTop.jsx
@@ -2,12 +2,12 @@ import '../index.css'
 function HowToCardTop() {
     return (
       <div className="HowToCardTop-container">
-        <svg className='HowToCardTop-svg-right' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 625 385" fill="none">
+        <svg className='HowToCardTop-svg-right' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 625 385" >
           <path d="M0 0H625L351.781 385H0V0Z" fill="url(#paint0_linear_399_722)" fillOpacity="0.7"/>
           <text  className="HowToCardTop-text" x="5%" y="40%"  fill="#050E15" fontSize={24}  color="#050E15">
             BUY 
           </text>
-          <text className="HowToCardTop-text" x="10%" y="75%"  fill="#050E15"   color="#050E15">
+          <text className="HowToCardTop-text" x="5%" y="75%"  fill="#050E15"   color="#050E15">
             CARDS
           </text>
           
@@ -24,14 +24,15 @@ function HowToCardTop() {
             <text className="HowToCardTop-text-2 text1" x="35%" y="14%"  fill="#EDB72D"   color="#EDB72D">
               Get the best deals  
             </text>
-          <svg className='FlechaParabajo-top-1'x="53%" y="17%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="110" height="110">
-            <path fill="none" stroke="#c78d18 "  strokeLinecap="round" strokeLinejoin="round" d="M19.09 3.5h0a1.41 1.41 0 0 0-1 .41L7 15l.41-6.91A1.51 1.51 0 0 0 5.9 6.5h0a1.51 1.51 0 0 0-1.5 1.4l-.9 12.6 12.6-.9a1.51 1.51 0 0 0 1.4-1.5h0a1.51 1.51 0 0 0-1.59-1.5L9 17 20.09 5.91a1.41 1.41 0 0 0 .41-1h0a1.41 1.41 0 0 0-1.41-1.41Z" data-name="Arrow 9 Right Down" ></path></svg>
+            
+          <svg className='FlechaParabajo-top-1'x="55%" y="22%"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="60" height="60">
+            <path fill="none" stroke="#c78d18"  strokeLinecap="round" strokeLinejoin="round" d="M19.09 3.5h0a1.41 1.41 0 0 0-1 .41L7 15l.41-6.91A1.51 1.51 0 0 0 5.9 6.5h0a1.51 1.51 0 0 0-1.5 1.4l-.9 12.6 12.6-.9a1.51 1.51 0 0 0 1.4-1.5h0a1.51 1.51 0 0 0-1.59-1.5L9 17 20.09 5.91a1.41 1.41 0 0 0 .41-1h0a1.41 1.41 0 0 0-1.41-1.41Z" data-name="Arrow 9 Right Down" ></path></svg>
 
           <text className="HowToCardTop-text-2 text2" x="20%" y="55%"  fill="#EDB72D"   color="#EDB72D">
           Enjoy the Gathering!
           </text>
          
-          <svg className='FlechaParabajo-top-2'x="30.5%" y="57%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="110" height="110">
+          <svg className='FlechaParabajo-top-2'x="34%" y="64%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="60" height="60">
             <path fill="none" stroke="#c78d18 "  strokeLinecap="round" strokeLinejoin="round" d="M19.09 3.5h0a1.41 1.41 0 0 0-1 .41L7 15l.41-6.91A1.51 1.51 0 0 0 5.9 6.5h0a1.51 1.51 0 0 0-1.5 1.4l-.9 12.6 12.6-.9a1.51 1.51 0 0 0 1.4-1.5h0a1.51 1.51 0 0 0-1.59-1.5L9 17 20.09 5.91a1.41 1.41 0 0 0 .41-1h0a1.41 1.41 0 0 0-1.41-1.41Z" data-name="Arrow 9 Right Down" ></path></svg>
 
 
