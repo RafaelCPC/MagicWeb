53989730531838110a35af4a5c6f42c18dcd1f96
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-03-15 15:49:00 +0100

    pequeño cambio

--- src/components/NavbarIcons.jsx
+++ src/components/NavbarIcons.jsx
@@ -12,7 +12,7 @@ function NavbarIcons({onIconsClick,stateIcons,userLogged}) {
   
   return (
     <div className="navbar-icons">
-      <Boton callback={()=>handleIconClick({isNotifications:!stateIcons.isNotifications,isCart:false,isUser:false,isMenu:false})} text={<NotificationsIcon/>}/>
+      <Boton callback={()=>handleIconClick({isNotifications:!stateIcons.isNotifications,isCart:false,isUser:false,isMenu:false})} text={<NotificationsIcon />}/>
       <Boton callback={()=>handleIconClick({isNotifications:false,isCart:!stateIcons.isCart,isUser:false,isMenu:false})} text={<CartIcon/>}/>
       {!userLogged && <Boton callback={()=>handleIconClick({isNotifications:false,isCart:false,isUser:!stateIcons.isUser,isMenu:false})} text={<UserIcon/>}/>}
       {userLogged && <Boton callback={()=>handleIconClick({isNotifications:false,isCart:false,isUser:false,isMenu:!stateIcons.isMenu})} text={<BurguerMenuIcon/>}/>}
