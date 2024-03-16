85600e12abaf165a8c7735522294ff635df3874c
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-03-16 11:25:42 +0100

    cambios

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import OnSaleProducts from './OnSaleProducts'
 import HowTo from './HowTo'
+import Banner from './Banner'
+import { TopPrices } from './TopPrices'
 
 
 function MainContent({states,callbacks}) {
   return (
     <div className='section-main-content'>
       
-      
+      <Banner />
+      <TopPrices title="TOP PRICES" />
       <HowTo callbacks={()=>{callbacks.setMessageIcon(prev=>{return({...prev,["isUser"]:true})})}}/>
       <OnSaleProducts states={states} callbacks={callbacks}/>
 
